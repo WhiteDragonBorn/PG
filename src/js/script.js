b8696bf534ea7780247f4a7c9d45fd82890e320a
WhiteDragonBorn <shimanoee@yandex.ru>
2020-08-30 13:29:36 +0300

    prod

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('hello')
+    const hidden = document.querySelector('.logo__hidden'),
+          title = document.querySelector('.logo__title'),
+          prodText = document.querySelector('.logo__hidden-text');
 
+        title.addEventListener('mouseover', (e) => {
+
+            hidden.classList.add('logo__hidden--transform');
+            prodText.classList.add('logo__hidden--color');
+
+        });
 
 
 
